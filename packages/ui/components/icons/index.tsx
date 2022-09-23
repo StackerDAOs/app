59baa6f77ac2afb0eb67e0bbc03d7abdf3143478
Ryan <ryan.waits@gmail.com>
2022-09-22 19:38:21 -0500

    add chevron icon

--- packages/ui/components/icons/index.tsx
+++ packages/ui/components/icons/index.tsx
@@ -1,6 +1,7 @@
 export * from './ArrowRight';
 export * from './CheckCircle';
 export * from './ChevronDown';
+export * from './ChevronDoubleRight';
 export * from './CopyClipboard';
 export * from './LightningBolt';
 export * from './LogoIcon';
