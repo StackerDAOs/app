d8cdbbd856bd3efa5e5b2312370abf5f9ab14791
Ryan <ryan.waits@gmail.com>
2022-09-27 16:35:40 -0500

    more env testing

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -6,5 +6,8 @@ module.exports = withTM({
     NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
     NEXT_PUBLIC_NEXTAUTH_URL: process.env.NEXT_PUBLIC_NEXTAUTH_URL,
     NEXT_PUBLIC_NEXTAUTH_SECRET: process.env.NEXT_PUBLIC_NEXTAUTH_SECRET,
+    NODE: process.env.NODE_ENV,
+    VERCEL: process.env.VERCEL,
+    SUP: process.env.NEXT_PUBLIC_NEXTAUTH_URL,
   },
 });
