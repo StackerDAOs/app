35d5aefa1df859983a55c70bb9826d29bbc8509e
Ryan <ryan.waits@gmail.com>
2022-10-19 17:54:28 -0500

    remove micro-stacks from utils dep

--- packages/ui/components/index.tsx
+++ packages/ui/components/index.tsx
@@ -2,6 +2,7 @@ export * from '@micro-stacks/react';
 export * from 'micro-stacks/network';
 export * from 'micro-stacks/clarity';
 export * from 'micro-stacks/connect';
+export * from 'micro-stacks/transactions';
 export { Step, Steps, useSteps } from 'chakra-ui-steps';
 export { useForm, Controller } from 'react-hook-form';
 export { default as useFormPersist } from 'react-hook-form-persist';
