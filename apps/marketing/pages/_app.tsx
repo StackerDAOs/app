7a71a384ba9bc55f7ef06136201687d0bbbca8d4
Ryan Waits <ryan.waits@gmail.com>
2022-12-22 11:37:23 -0700

    add og meta tags

--- apps/marketing/pages/_app.tsx
+++ apps/marketing/pages/_app.tsx
@@ -12,6 +12,15 @@ function App({ Component, pageProps }: any) {
         <meta name='description' content='StackerDAOs' />
         <meta name='viewport' content='initial-scale=1.0, width=device-width' />
         <meta name='image' content='/meta-image.png' />
+        <meta property='og:image' content='/meta-image.png' />
+        {/* <meta property='og:image:width' content='<image width>' /> */}
+        {/* <meta property='og:image:height' content='<image height>' /> */}
+        <meta property='og:title' content='StackerDAO Labs' />
+        <meta
+          property='og:description'
+          content='Take ownership & action. Together.'
+        />
+        <meta property='og:url' content='https://stackerdaos.com' />
         <link rel='icon' sizes='192x192' href='/android-chrome-192x192.png' />
         <link rel='icon' sizes='512x512' href='/android-chrome-512x512.png' />
         <link
