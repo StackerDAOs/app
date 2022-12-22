548cd7b07a09153a719075c9dde0ec5f3bc0ebea
Ryan Waits <ryan.waits@gmail.com>
2022-12-22 13:03:53 -0700

    add STACKS_NETWORK to next config

--- apps/marketing/next.config.js
+++ apps/marketing/next.config.js
@@ -3,6 +3,6 @@ const withTM = require('next-transpile-modules')(['ui', 'utils', 'api']);
 module.exports = withTM({
   reactStrictMode: true,
   env: {
-    API_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    STACKS_NETWORK: process.env.STACKS_NETWORK,
   },
 });
